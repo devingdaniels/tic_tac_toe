5b98ded2ea745f39125499b1b6ef19fbcd840528
Devin Daniels <devingdaniels@gmail.com>
2022-08-09 15:29:53 -0700

    HTML structure and CSS styling are complete

---
+++ script.js
@@ -0,0 +1,33 @@
+const gameBoardContainer = document.querySelector('gameBoardContainer')
+
+
+// OBJECTS
+
+/*
+Game board
+- Array  with 9 squares 
+- Each square is a div element ability to hold 'x' or 'O'
+- Each square has event listener for on click, if empty, add current player symbol  
+- 
+
+
+
+Player
+    - each player needs a symbol
+    - 
+*/
+
+
+
+
+//  Functions 
+
+/*
+checkWin() - first check for win 
+checkTie() -- if win returns false, check for draw 
+resetGame() - wipe the board
+renderBoard() - create 
+
+
+
+*/
